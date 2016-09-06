6d8424a710265f74601dd4b7223f12ca1b300e70
Dale Beran <daleberan@gmail.com>
2016-09-06 11:14:38 -0400

    the game

---
+++ Assets/Move_simple1.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_simple1 : MonoBehaviour {
+
+	public float speed1 = 2;
+
+	private Rigidbody rb;
+
+	public float MoveHorizontal; 
+	public float MoveVertical; 
+
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
+		
+	void FixedUpdate () 
+	{
+
+		MoveHorizontal = 0;
+		MoveVertical = 0;
+
+		if (Input.GetKey (KeyCode.RightArrow))
+			   MoveHorizontal = 1; 
+		
+		if (Input.GetKey (KeyCode.LeftArrow))
+				MoveHorizontal = -1; 
+		
+		if (Input.GetKey (KeyCode.UpArrow))
+				MoveVertical = 1; 
+
+		if (Input.GetKey (KeyCode.DownArrow))
+				MoveVertical = -1; 
+
+
+		Vector3 movement = new Vector3 (MoveHorizontal, 0.0f, MoveVertical);
+
+		rb.AddForce(movement*speed1);
+
+	}
+
+}
---
+++ Assets/script/Player_movement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player_movement : MonoBehaviour 
+{
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+
+	}
+
+
+	void FixedUpdate () 
+	{
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement*speed);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("pickup"))
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 4) 
+		{
+			winText.text = "You Win!";
+		}
+	}
+
+}
